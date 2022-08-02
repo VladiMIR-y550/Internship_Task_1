f1b8164792607c2f14043b07f61344ec6a2d441d
VladiMIR-y550 <mironenko.vladimir2019@gmail.com>
2022-08-02 13:59:04 +0300

    refactor AppService: delete START_STICKY, StartForegroundProcess() in application class

--- app/src/main/java/com/mironenko/internship_task_1/InternshipTask1App.kt
+++ app/src/main/java/com/mironenko/internship_task_1/InternshipTask1App.kt
@@ -2,11 +2,14 @@ package com.mironenko.internship_task_1
 
 import android.app.Application
 import android.content.Intent
+import android.os.Build
 
 class InternshipTask1App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        startService(Intent(this, AppService::class.java))
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(Intent(this, AppService::class.java))
+        }
     }
 }
